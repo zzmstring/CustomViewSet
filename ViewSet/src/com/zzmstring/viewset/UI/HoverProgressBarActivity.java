9d759685eb3b4f83aa8a906fd85dac760ad6704f
zzmxstring <zzmx9891@126.com>
2015-02-01 17:36:47 +0800

    test push

---
+++ ViewSet/src/com/zzmstring/viewset/UI/HoverProgressBarActivity.java
@@ -0,0 +1,31 @@
+package com.zzmstring.viewset.UI;
+
+import android.os.Bundle;
+
+import com.zzmstring.viewset.Base.BaseActivity;
+
+/**
+ * Created by zzmx on 2015/2/1.
+ */
+public class HoverProgressBarActivity extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+}
