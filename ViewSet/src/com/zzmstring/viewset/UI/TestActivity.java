aa2c6e332d88bc84660e2ef389799b9ba4320aa9
zzmstring <ac8512@126.com>
2015-02-05 14:24:32 +0800

    gitceshi1

---
+++ ViewSet/src/com/zzmstring/viewset/UI/TestActivity.java
@@ -0,0 +1,10 @@
+package com.zzmstring.viewset.UI;
+
+import com.zzmstring.viewset.Base.BaseActivity;
+
+/**
+ * Created by zzmstring on 2015/2/5.
+ */
+public class TestActivity extends BaseActivity {
+
+}
