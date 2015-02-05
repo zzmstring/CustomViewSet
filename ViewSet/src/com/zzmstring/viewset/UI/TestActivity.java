46ebafe73a7ae10e2978b5731b2c0b65d735082b
zzmstring <ac8512@126.com>
2015-02-05 14:26:17 +0800

    gitceshi2

--- ViewSet/src/com/zzmstring/viewset/UI/TestActivity.java
+++ ViewSet/src/com/zzmstring/viewset/UI/TestActivity.java
@@ -1,10 +1,16 @@
 package com.zzmstring.viewset.UI;
 
+import android.os.Bundle;
+
 import com.zzmstring.viewset.Base.BaseActivity;
 
 /**
  * Created by zzmstring on 2015/2/5.
  */
 public class TestActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
 }
