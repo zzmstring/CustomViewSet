db849d4f15a5fe14c8d1491d9cd3ebf2d8cc6cd5
zzmstring <ac8512@126.com>
2015-02-28 23:20:44 +0800

    heihei

---
+++ ViewSet/src/com/zzmstring/viewset/UI/ToolBarActivity.java
@@ -0,0 +1,30 @@
+package com.zzmstring.viewset.UI;
+
+import android.os.Bundle;
+
+import com.zzmstring.viewset.Base.BaseActivity;
+
+/**
+ * Created by zzmstring on 2015/2/28.
+ */
+public class ToolBarActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+}
