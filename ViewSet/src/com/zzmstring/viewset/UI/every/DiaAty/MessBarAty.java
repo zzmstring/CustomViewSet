a5ec08a3ef5421e8ee898391d3c89253a88bf0c0
zzmstring <ac8512@126.com>
2015-05-04 19:41:29 +0800

    0504

--- ViewSet/src/com/zzmstring/viewset/UI/every/DiaAty/MessBarAty.java
+++ ViewSet/src/com/zzmstring/viewset/UI/every/DiaAty/MessBarAty.java
@@ -1,14 +1,26 @@
 package com.zzmstring.viewset.UI.every.DiaAty;
 
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
+import android.widget.TextView;
 
 import com.zzmstring.viewset.Base.BaseActivity;
+import com.zzmstring.viewset.R;
+import com.zzmstring.viewset.View.messagebar.MessageBar;
 
 /**
  * Created by zzmstring on 2015/5/3.
  */
-public class MessBarAty extends BaseActivity {
+public class MessBarAty extends BaseActivity implements MessageBar.OnMessageClickListener {
+    private static final String STATE_MESSAGEBAR = "net.simonvt.messagebar.samples.SampleActivity.messageBar";
+    private static final String STATE_COUNT = "net.simonvt.messagebar.samples.SampleActivity.count";
+
+    private MessageBar mMessageBar;
+
+    private TextView mTextView;
+
+    private int mCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +28,29 @@ public class MessBarAty extends BaseActivity {
 
     @Override
     public void initView() {
+        setContentView(R.layout.messbar_main);
+        mTextView = (TextView) findViewById(R.id.messageClickedTextView);
 
+        mMessageBar = new MessageBar(this);
+        mMessageBar.setOnClickListener(this);
+
+        findViewById(R.id.withText).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMessageBar.show("Message #" + mCount);
+                mCount++;
+            }
+        });
+
+        findViewById(R.id.withTextAndButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle b = new Bundle();
+                b.putInt("count", mCount);
+                mMessageBar.show("Message #" + mCount, "Button!", R.drawable.ic_messagebar_undo, b);
+                mCount++;
+            }
+        });
     }
 
     @Override
@@ -32,5 +66,11 @@ public class MessBarAty extends BaseActivity {
     @Override
     public void onClick(View v) {
 
+    }
+    @Override
+    public void onMessageClick(Parcelable token) {
+        Bundle b = (Bundle) token;
+        final int count = b.getInt("count");
+        mTextView.setText("You clicked message #" + count);
     }
 }
