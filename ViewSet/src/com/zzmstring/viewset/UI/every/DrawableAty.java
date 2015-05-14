ab6d7f710e503947db4c8d5bcec6b3645ec8acb1
zzmstring <ac8512@126.com>
2015-05-14 23:52:47 +0800

    添加自定义添加imageview

---
+++ ViewSet/src/com/zzmstring/viewset/UI/every/DrawableAty.java
@@ -0,0 +1,31 @@
+package com.zzmstring.viewset.UI.every;
+
+import android.os.Bundle;
+
+import com.zzmstring.viewset.Base.BaseActivity;
+
+/**
+ * Created by zzmstring on 2015/5/14.
+ */
+public class DrawableAty extends BaseActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void initView() {
+
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initListener() {
+
+    }
+
+}
--- ViewSet/src/com/zzmstring/viewset/View/my/RoundImageDrawable.java
+++ ViewSet/src/com/zzmstring/viewset/View/my/RoundImageDrawable.java
@@ -5,6 +5,7 @@ import android.graphics.BitmapShader;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
 import android.graphics.Paint;
+import android.graphics.PixelFormat;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
@@ -27,22 +28,39 @@ public class RoundImageDrawable extends Drawable {
         mPaint.setShader(bitmapShader);
     }
     @Override
+    public void setBounds(int left, int top, int right, int bottom)
+    {
+        super.setBounds(left, top, right, bottom);
+        rectF = new RectF(left, top, right, bottom);
+    }
+    @Override
     public void draw(Canvas canvas) {
-
+        canvas.drawRoundRect(rectF, 30, 30, mPaint);
+    }
+    @Override
+    public int getIntrinsicWidth()
+    {
+        return mBitmap.getWidth();
     }
 
     @Override
+    public int getIntrinsicHeight()
+    {
+        return mBitmap.getHeight();
+    }
+    @Override
     public void setAlpha(int i) {
+        mPaint.setAlpha(i);
 
     }
 
     @Override
     public void setColorFilter(ColorFilter colorFilter) {
-
+        mPaint.setColorFilter(colorFilter);
     }
 
     @Override
     public int getOpacity() {
-        return 0;
+        return PixelFormat.TRANSLUCENT;
     }
 }
