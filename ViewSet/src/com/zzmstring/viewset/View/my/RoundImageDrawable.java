91baab87456be81effabae5188e33cc0f4fde59b
zzmstring <ac8512@126.com>
2015-05-14 23:43:33 +0800

    添加自定义drawable

---
+++ ViewSet/src/com/zzmstring/viewset/View/my/RoundImageDrawable.java
@@ -0,0 +1,48 @@
+package com.zzmstring.viewset.View.my;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.graphics.Shader;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by zzmstring on 2015/5/14.
+ */
+public class RoundImageDrawable extends Drawable {
+    private Paint mPaint;
+    private Bitmap mBitmap;
+
+    private RectF rectF;
+    public RoundImageDrawable(Bitmap bitmap)
+    {
+        mBitmap = bitmap;
+        BitmapShader bitmapShader = new BitmapShader(bitmap, Shader.TileMode.CLAMP,
+                Shader.TileMode.CLAMP);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setShader(bitmapShader);
+    }
+    @Override
+    public void draw(Canvas canvas) {
+
+    }
+
+    @Override
+    public void setAlpha(int i) {
+
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter colorFilter) {
+
+    }
+
+    @Override
+    public int getOpacity() {
+        return 0;
+    }
+}
