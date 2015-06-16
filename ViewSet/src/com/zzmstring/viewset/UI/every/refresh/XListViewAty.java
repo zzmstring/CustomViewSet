f9a3a65d8031d05aebc7e25df51052c128fd927a
zzmstring <ac8512@126.com>
2015-06-16 23:25:56 +0800

    add aty

--- ViewSet/src/com/zzmstring/viewset/UI/every/refresh/XListViewAty.java
+++ ViewSet/src/com/zzmstring/viewset/UI/every/refresh/XListViewAty.java
@@ -3,12 +3,20 @@ package com.zzmstring.viewset.UI.every.refresh;
 import android.os.Bundle;
 import android.view.View;
 
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.zzmstring.viewset.Adapter.TestAdapter;
 import com.zzmstring.viewset.Base.BaseActivity;
+import com.zzmstring.viewset.R;
+import com.zzmstring.viewset.View.XListView.XListView;
 
 /**
  * Created by zzmstring on 2015/6/16.
  */
-public class XListViewAty extends BaseActivity {
+public class XListViewAty extends BaseActivity implements XListView.IXListViewListener {
+    @ViewInject(R.id.lv_list)
+    XListView lv_list;
+    TestAdapter mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +24,13 @@ public class XListViewAty extends BaseActivity {
 
     @Override
     public void initView() {
-
+        setContentView(R.layout.aty_xlistview);
+        ViewUtils.inject(this);
+        lv_list.setXListViewListener(this);
+        lv_list.setPullLoadEnable(true);
+        lv_list.setPullRefreshEnable(true);
+        mAdapter=new TestAdapter(this);
+        lv_list.setAdapter(mAdapter);
     }
 
     @Override
@@ -31,6 +45,16 @@ public class XListViewAty extends BaseActivity {
 
     @Override
     public void onClick(View v) {
+
+    }
+
+    @Override
+    public void onRefresh() {
+
+    }
+
+    @Override
+    public void onLoadMore() {
 
     }
 }
